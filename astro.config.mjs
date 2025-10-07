2fb4db34c7c6499efeb4908994a30cfc40016ef7
Jacob Haugen <howagain@gmail.com>
2025-10-07 16:43:19 -0500

    initial setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import node from '@astrojs/node';
+
+import solidJs from '@astrojs/solid-js';
+
+import db from '@astrojs/db';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  adapter: node({
+    mode: 'standalone'
+  }),
+
+  integrations: [solidJs(), db()]
+});
